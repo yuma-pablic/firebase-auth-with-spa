e25a398817f6fadf020865efac9a09ea4a45d0b4
yuma-pablic <yuumanagai0504pablic@gmail.com>
2024-06-02 22:38:25 +0900

    fe

--- api/usecase/login_use_case.go
+++ api/usecase/login_use_case.go
@@ -18,12 +18,12 @@ func NewLoginUserUseCase(ur userDomain.UserRepository) *LoginUserUseCase {
 	return &LoginUserUseCase{ur}
 }
 
-func (uc *LoginUserUseCase) Run(ctx context.Context) (*FindUserUseCaseOutputDTO, error) {
+func (uc *LoginUserUseCase) Run(ctx context.Context) (*LoginUserUseCaseOutputDTO, error) {
 	user, err := uc.ur.Find()
 	if err != nil {
 		return nil, err
 	}
-	return &FindUserUseCaseOutputDTO{
+	return &LoginUserUseCaseOutputDTO{
 		ID:    user.ID,
 		Email: user.Email,
 		Type:  user.Type,
